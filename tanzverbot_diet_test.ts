6a52c0736bd3cef0cfc16c5db872a218bd859125
D7724 <Ryan_Truttmann@sluz.ch>
2026-05-22 11:05:20 +0200

    Aufgabe 2

--- tanzverbot_diet_test.ts
+++ tanzverbot_diet_test.ts
@@ -1,6 +1,10 @@
-import { assertGreater } from "@std/assert";
+import { assertEquals, assertGreater } from "@std/assert";
 import { calcDateOnDiet, Sex } from "./tanzverbot_diet.ts";
 
 Deno.test("Tanzverbot Diet", () => {
   assertGreater(calcDateOnDiet(74, 100, 1.86, 38, Sex.Male), 0.0);
+});
+
+Deno.test("Tanzverbot Diet Female", () => {
+  assertEquals(calcDateOnDiet(74, 100, 1.86, 38, Sex.Female), 35);
 });
